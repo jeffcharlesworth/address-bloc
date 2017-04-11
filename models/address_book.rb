cd89a8eedd9b9e33a80c39cf1036446c5b8db29a
Jeffrey C. Charlesworth <jcharlesworth02@gmail.com>
2017-04-10 17:37:52 -0700

    Address bloc 7 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,13 +1,14 @@
 
 require_relative 'entry'
+require "csv"
 
 class AddressBook
     attr_reader :entries
-   
-   def initialize 
+
+   def initialize
        @entries = []
    end
-   
+
    def add_entry(name, phone_number, email)
        index = 0
        entries.each do |entry|
@@ -17,5 +18,15 @@ class AddressBook
            index+= 1
        end
        entries.insert(index, Entry.new(name, phone_number, email))
+   end
+
+   def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
    end
 end
